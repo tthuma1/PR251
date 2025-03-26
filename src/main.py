ab345ae9367cce2d5fb2ddcbffa0cb9d52c5c9c1
Tim Thuma <thumatim@gmail.com>
2025-03-26 10:39:29 +0100

    update data reads

--- src/main.py
+++ src/main.py
@@ -1,15 +1,18 @@
 from pyaxis import pyaxis
 import pandas as pd
 
-pd.set_option('display.max_rows', 500)
+# pd.set_option('display.max_rows', 500)
 
-# EXAMPLE_URL = 'data/prve_registracije.PX'
+EXAMPLE_URL = '../data/prve_registracije.PX'
 
-# px = pyaxis.parse(EXAMPLE_URL, encoding='ISO-8859-2')
+# px = pyaxis.parse(EXAMPLE_URL, encoding='cp1252')
 # print(px['DATA'].head(100))
 
 # df = pd.read_csv('../data/nesrece/pn2009.csv', encoding='cp1252', sep=';')
 # print(df.head(100))
 
-df = pd.read_excel('../data/prometne_obremenitve/pldp2020noo.xlsx')
+df = pd.read_excel('../data/prometne_obremenitve/pldp2016noo.xlsx')
 print(df.head(100))
+
+# df = pd.read_excel('../data/prometne_obremenitve/pldp2010noo.xls')
+int(df.head(100))
