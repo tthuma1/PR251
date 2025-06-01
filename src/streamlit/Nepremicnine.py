5df087fae6e963875e37b6723713e40300ce5dd4
Tim Thuma <30806431+tthuma1@users.noreply.github.com>
2025-06-01 17:38:42 +0200

    updated app title

--- src/streamlit/Nepremicnine.py
+++ src/streamlit/Nepremicnine.py
@@ -1,8 +1,7 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Hello",
-    page_icon="👋",
+    page_title="PR Nepremicnine",
 )
 
 st.write("# Pozdravljen na naši streamlit aplikaciji! 👋")
